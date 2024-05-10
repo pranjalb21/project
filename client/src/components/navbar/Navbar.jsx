f5f7ada541115687f5f9cc49ccb18520414ff6c4
Pranjal Gain <pranjalgain052@gmail.com>
2024-05-11 04:23:41 +0530

    Responsive Navbar implemented.

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,7 +2,9 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom'
 import { selectLogin, userLogout } from '../../store/authSlice';
-// import './navbar.css';
+
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
 const Navbar = () => {
   const isLoggedIn = useSelector(selectLogin);
   const dispatch = useDispatch()
@@ -23,8 +25,65 @@ const Navbar = () => {
             </div>
           </div>
         </NavLink>
-        <nav className="navlinks">
+        <nav className="navlink-desktop-container">
           <ul>
+            <NavLink to={'/'} className={({ isActive, isPending }) =>
+              isPending ? "pending navlink" : isActive ? "active navlink" : " navlink"
+            }>
+              <li>Home</li>
+            </NavLink>
+            <NavLink to={'/about'} className={({ isActive, isPending }) =>
+              isPending ? "pending navlink" : isActive ? "active navlink" : " navlink"
+            }>
+              <li>About</li>
+            </NavLink>
+            <NavLink to={'/contact'} className={({ isActive, isPending }) =>
+              isPending ? "pending navlink" : isActive ? "active navlink" : " navlink"
+            }>
+              <li> Contact</li>
+            </NavLink>
+            {/* <NavLink to={'/services'} className={({ isActive, isPending }) =>
+              isPending ? "pending navlink" : isActive ? "active navlink" : " navlink"
+            }>
+              <li>Services</li>
+            </NavLink> */}
+            {isLoggedIn ?
+              <>
+                <NavLink to={'/dashboard'} className={({ isActive, isPending }) =>
+                  isPending ? "pending navlink" : isActive ? "active navlink" : " navlink"
+                }>
+                  <li>My Dashboard</li>
+                </NavLink>
+                <NavLink to={'/login'} className={({ isActive, isPending }) =>
+                  isPending ? "pending navlink" : isActive ? "active navlink" : " navlink"
+                } onClick={handleLogout}>
+                  <li> Logout</li>
+                </NavLink>
+              </> :
+              <>
+                <NavLink to={'/login'} className={({ isActive, isPending }) =>
+                  isPending ? "pending navlink" : isActive ? "active navlink" : " navlink"
+                }>
+                  <li>Login</li>
+                </NavLink>
+                <NavLink to={'/register'} className={({ isActive, isPending }) =>
+                  isPending ? "pending navlink" : isActive ? "active navlink" : " navlink"
+                }>
+                  <li>Register</li>
+                </NavLink>
+              </>
+            }
+
+
+          </ul>
+        </nav>
+        <nav className="navlink-mobile-container">
+          
+          <input type="checkbox" name="nav" id="nav" />
+          <label id='ham' htmlFor="nav"><GiHamburgerMenu/></label>
+          <label id='outside' htmlFor="nav"></label>
+          <ul>
+          <label htmlFor="nav"><IoClose /></label>
             <NavLink to={'/'} className={({ isActive, isPending }) =>
               isPending ? "pending navlink" : isActive ? "active navlink" : " navlink"
             }>
