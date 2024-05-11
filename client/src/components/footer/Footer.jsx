43e18978e3ad7be80bfc46f3207d841bd376b8bd
Pranjal Gain <pranjalgain052@gmail.com>
2024-05-11 05:38:06 +0530

    Basic footer has been added. and styling has been done.

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer-container mt-lg'>
+      <p>Developed by Pranjal Gain</p>
+      <p>Copyright &copy; 2024-25</p>
+    </div>
+  )
+}
+
+export default Footer
