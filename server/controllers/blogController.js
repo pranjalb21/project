cbae687ed670fc6c0edf8008b78cb2c3ae50d946
Pranjal Gain <pranjalgain052@gmail.com>
2024-05-12 06:35:52 +0530

    Image upload into S3 backend has been created. Fetching image from S3 int blogs has been implemented.

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,6 +1,22 @@
-const { default: mongoose } = require("mongoose");
 const Blog = require("../models/blogModel")
 const upload = require('../middlewares/imageUploadMiddleware')
+const mongoose = require("mongoose");
+const { PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const sharp = require('sharp');
+const s3 = require("../utils/aws-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+
+const generateURL = async (imageName) => {
+    const bucketName = process.env.AWS_S3_BUCKET;
+    //Generate image URL using image name
+    const getObjectParams = {
+        Bucket: bucketName,
+        Key: imageName
+    }
+    const getCommand = new GetObjectCommand(getObjectParams);
+    const url = await getSignedUrl(s3, getCommand, { expiresIn: 86400 });
+    return url;
+}
 
 const AddBlog = async (req, res) => {
     try {
@@ -20,8 +36,16 @@ const AddBlog = async (req, res) => {
 const GetAllBlog = async (req, res) => {
     try {
         const result = await Blog.find({}).sort({ createdAt: -1 }).populate('userId');
+        const x = [];
+        for(const blog of result){
+            const url = await generateURL(blog.image)
+            x.push({
+                ...blog._doc,
+                imageURL: url
+            })
+        }
         if (result.length > 0)
-            res.status(200).send({ message: `Blogs has been fetched`, data: result });
+            res.status(200).send({ message: `Blogs has been fetched`, data: x });
         else
             res.status(200).send({ message: `No blogs to show.` });
 
@@ -30,22 +54,21 @@ const GetAllBlog = async (req, res) => {
     }
 }
 
-const UploadImage = async (req, res) => {
-    if (req.file) {
-        res.status(201).send(req.file.filename);
-    } else {
-        res.status(400).send({ message: `Please select an image for upload.` });
-
-    }
-}
-
 const GetBlogByUserId = async (req, res) => {
     try {
         const { user } = req.body;
         const blogs = await Blog.find({ userId: user }).sort({ createdAt: -1 });
+        const x = [];
+        for(const blog of blogs){
+            const url = await generateURL(blog.image)
+            x.push({
+                ...blog._doc,
+                imageURL: url
+            })
+        }
         if (blogs.length > 0) {
-            res.status(200).send({ message: `Blogs has been fetched`, data: {blogs, favourites:[]} });
-        }else{
+            res.status(200).send({ message: `Blogs has been fetched`, data: { blogs: x, favourites: [] } });
+        } else {
             res.status(200).send({ message: `No blogs to show.` });
         }
     } catch (error) {
@@ -53,4 +76,54 @@ const GetBlogByUserId = async (req, res) => {
     }
 }
 
-module.exports = { AddBlog, GetAllBlog, UploadImage, GetBlogByUserId }
+const UploadBlog = async (req, res) => {
+    try {
+        const bucketName = process.env.AWS_S3_BUCKET;
+
+        //Edit image dimention and type
+        const buffer = await sharp(req.file.buffer)
+            .resize({
+                width: 1200,
+                height: 630,
+                fit: sharp.fit.fill
+            })
+            .jpeg({
+                quality: 100,
+                chromaSubsampling: '4:4:4'
+            })
+            .toBuffer();
+
+        //Create unique image name
+        const uniqueFileName = Date.now() + '-' + Math.round(Math.random() * 1E9) + '.jpeg'
+
+        //Upload the image to AWS S3
+        const params = {
+            Bucket: bucketName,
+            Key: uniqueFileName,
+            Body: buffer,
+            ContentType: req.file.mimetype
+        }
+
+        const putCommand = new PutObjectCommand(params);
+        await s3.send(putCommand);
+
+        //Save the image name and other form data into Database
+        const result = await Blog.create({
+            userId: new mongoose.Types.ObjectId(req.body.userId),
+            title: req.body.title,
+            image: uniqueFileName,
+            description: req.body.description
+        });
+
+        const data = {
+            ...result._doc,
+            imageURL: await generateURL(result.image)
+        }
+        res.status(201).send({ message: `Blog has been posted successfully.`, data });
+    } catch (error) {
+        res.status(500).send({ message: `Something went wrong. Please try again.` });
+    }
+}
+
+
+module.exports = { AddBlog, GetAllBlog, UploadBlog, GetBlogByUserId }
--- server/middlewares/imageUploadMiddleware.js
+++ server/middlewares/imageUploadMiddleware.js
@@ -1,44 +1,7 @@
 const multer = require('multer');
-const aws = require('aws-sdk');
-const multerS3 = require('multer-s3');
-
-const s3 = new aws.S3({
-    accessKeyId: process.env.S3_ACCESS_KEY,
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    region: process.env.S3_REGION
-})
 
 
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, '../client/public/uploads/')
-//     },
-//     filename: function (req, file, cb) {
-//         console.log(req.body);
-//         console.log(file);
-//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + '.' +
-//             file.originalname.split('.')[file.originalname.split('.').length - 1]
-//         cb(null, uniqueSuffix)
-//     }
-// })
-
-
-
-// const upload = multer({ storage: storage });
-
-const upload = multer({
-    storage: multerS3({
-        s3,
-        bucket: "travelblog-p ",
-        metadata: function (req, file, cb) {
-            cb(null, { fieldname: file.fieldname })
-        },
-        key: function (req, file, cb) {
-            // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + '.' +
-            //     file.originalname.split('.')[file.originalname.split('.').length - 1]
-            cb(null, 'imag.jpg')
-        }
-    })
-});
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage});
 
 module.exports = upload;
